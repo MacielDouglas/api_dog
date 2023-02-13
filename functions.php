61741a2cbab291ffb6017a093433e5385e767d5e
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-02-13 10:36:16 -0300

    terminado post imagens, modificado tamanho upload

--- functions.php
+++ functions.php
@@ -1,13 +1,21 @@
 <?php 
-remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
+
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
 
+require_once $dirbase . '/endpoints/photo_post.php';
+
+// definir um tamanho específico de tamanho de imagem 
+update_option('large_size_w', 1000);
+update_option('large_size_h', 1000);
+update_option('large_crop', 1);
+//crop é para cortar a imagem e deixá-la sempre quadrada
 
 function change_api() {
-  return 'json';
+  return 'json'; 
 }
 add_filter('rest_url_prefix', 'change_api');
 
